268ce94968c13fe25712183e75efaba70bbd07ab
Jon Cucci <joncucci@gmail.com>
2021-10-02 16:46:05 -0400

    Addition of Assignment project

--- TestGetRepo.py
+++ TestGetRepo.py
@@ -8,32 +8,56 @@ class TestGetRepo(unittest.TestCase):
 
     def testValidRepoReturn(self):
 
-        expected = {
-            'Complexity': 46,
-            'CS559_MachineLearning': 8,
-            'data_analysis': 2,
-            'DesignPatternPractice': 1,
-            'first': 2,
-            'GEDCOM_database': 3,
-            'gemini_API': 1,
-            'HW3_345': 3,
-            'HW4-345': 5,
-            'HW4_345': 1,
-            'Is_It_Open': 2,
-            'joncucci': 7,
-            'Machine_Learning_Study': 18,
-            'NodeJS_app': 2,
-            'random_java': 2,
-            'random_python': 2,
-            'SSW345-FinalBot': 13,
-            'SSW345-HW5': 3,
-            'SSW345-HW5-1': 2,
-            'SSW533': 3,
-            'SSW567': 8,
-            'test-HW4-345': 2,
-            'Triangle567': 7,
-            'workspace': 1
-        }
+        expected =  {'CS559_MachineLearning': 8,
+
+   'Complexity': 46,
+
+   'DesignPatternPractice': 1,
+
+   'GEDCOM_database': 3,
+
+    'GitHubAPI567': 4,
+
+   'HW3_345': 3,
+
+   'HW4-345': 5,
+
+   'HW4_345': 1,
+
+   'Is_It_Open': 2,
+
+   'Machine_Learning_Study': 18,
+
+   'NodeJS_app': 2,
+
+   'SSW345-FinalBot': 13,
+
+   'SSW345-HW5': 3,
+
+   'SSW345-HW5-1': 2,
+
+   'SSW533': 3,
+
+   'SSW567': 8,
+
+   'Triangle567': 7,
+
+   'data_analysis': 2,
+
+   'first': 2,
+
+   'gemini_API': 1,
+
+   'joncucci': 7,
+
+   'random_java': 2,
+
+   'random_python': 2,
+
+   'test-HW4-345': 2,
+
+   'workspace': 1}
+
         self.assertEqual(get_repos('joncucci'), expected, 'Wrong repo and commit combination.')
 
     def testInvalidUser(self):
